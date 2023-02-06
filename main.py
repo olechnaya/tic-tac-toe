08353a3ed3829621155d08ede945d9fe23665669
Olga <data19101988@gmail.com>
2023-02-06 14:19:41 +0300

    files added main.py - tasks description show_playground

--- main.py
+++ main.py
@@ -2,7 +2,19 @@
 # БЛОК 2 Опрос пользователей (расстановка символов): по очереди ставим крестики/нолики в течение 9 ходов.
 # БЛОК 3 Кто выиграл? (Ничья или определение победителя)
 # БЛОК 4 Обеспечение взаимодествия всех трёх блоков
+
 if __name__ == '__main__':
-    print("Hello")
+    field = [['-']*3 for _ in range(3)]
+
+
+    def show_playground(f):
+        num = '  a b c'
+        print(num)
+        for row, i in zip(field, num.split()):
+            print(f'{i} {" ".join(str(j) for j in row)}')
+
+
+    show_playground(field)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
