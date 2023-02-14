8c20f9825cbfc64dd6d7c0ed0bcb40d1d78f1d37
Olga <data19101988@gmail.com>
2023-02-14 13:08:27 +0300

    code added

--- main.py
+++ main.py
@@ -3,18 +3,109 @@
 # БЛОК 3 Кто выиграл? (Ничья или определение победителя)
 # БЛОК 4 Обеспечение взаимодествия всех трёх блоков
 
-if __name__ == '__main__':
-    field = [['-']*3 for _ in range(3)]
+def show_playground(f):
+    # отрисовка именований колонок/столбцов
+    print('  1 2 3')
+    
+    # отрисовка построчно именование строки и три терешки
+    # 0 ---
+    # 1 ---
+    # 2 ---
 
 
-    def show_playground(f):
-        num = '  a b c'
-        print(num)
-        for row, i in zip(field, num.split()):
-            print(f'{i} {" ".join(str(j) for j in row)}')
+    #  Вариант 1
+    #     for i in range(len(field)):
+    #         print(str(i) + ' '.join(field[i]))
+
+    #  Вариант 2
+    #   for row, i in zip(field, num.split()):
+    #       print(f'{i} {" ".join(str(j) for j in row)}')
+
+    #  Вариант "примитив первого уровня"
+    for i in [0,1,2]:
+        print(i+1,' '.join(field[i]))
+    
+    #  Вариант "примитив второго уровня"
+    #   for i in range(3):
+    #     print(i,' '.join(field[i]))
+
+def user_input(f):
+   
+    while True:
+        # пользователь должен ввести обязательно два числа без пробела
+        coords = input('Введите координаты вашего хода в формате xy:  ')
+                
+        # проверка элементов - два? 
+        if len(coords) != 2:
+            print("Введите две координаты...")
+            continue
+       
+        # проверка элементов строки - это числа?
+        elif not(coords[0].isdigit() and coords[1].isdigit()):
+            print("Введите числа...")
+            continue
+           
+        # перевод строчных значений в численные
+        # используем map для применения функции int для каждого элемента списка
+    
+        x,y = map(int, coords)
+        
+        if not((x >= 1 and x < 4) and (y >= 1 and y < 4)):
+            print("Вы вышли из диапазона сетки. Введите числа от 0 до 2...")
+            continue
+        
+        
+        # проверка, что ячейка в поле f (переданный аргумент при вызове функции) не занята
+        if f[x-1][y-1] != '-':
+            print("Ячейка занята, попробуйте другие координаты...")
+            continue
+        break
+    return x-1,y-1
+
+def win_combo(f, user):
+
+    #локальная функция - проверка линии
+    def check_line(cell1, cell2, cell3, current_user):
+        if cell1 == current_user and cell2 == current_user and cell3 == current_user:
+            return True
+    
+    # в цикле для всех вариантов столбцов
+    for n in range(3):
+        # Ниже
+        # проверяем строки
+        # проверяем колонки
+        # проверяем диагонали
+        if check_line(f[n][0], f[n][1], f[n][2], user) or \
+            check_line(f[0][n], f[1][n], f[2][n], user) or \
+            check_line(f[0][0], f[1][1], f[2][2], user) or \
+            check_line(f[2][0], f[1][1], f[0][2], user):
+            return True
+    
+    return False
 
 
+
+count = 0
+#создание списка тирешек, выступающих в роли поля
+field = [['-']*3 for _ in range(3)] 
+
+while True:
+    
+    if count % 2 == 0:
+        user = 'x'
+    else:
+        user = 'o'
     show_playground(field)
+    x,y = user_input(field)
+    field[x][y] = user
 
+    if count == 9:
+        print("Игра окончена - ничья")
+    
+    if win_combo(field, user):
+        show_playground(field)
+        print(f"Пользователь {user} выиграл")
+        break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    count += 1
+        
